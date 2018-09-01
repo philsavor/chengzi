b49e0adc7d08881eda05adca8c2151f96b860784
System Administrator <root@Xiangyus-MacBook-Pro.local>
2018-09-01 17:42:39 +0800

    add gitignore

---
+++ backend/chengzi/src/main/java/com/sample/springboot/MyController.java
@@ -0,0 +1,18 @@
+package com.sample.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/postfeed")
+    @ResponseBody
+    public String postFeed(@RequestParam(value="feedId", required=false, defaultValue="abc") String feedId) {
+
+        return "feedId:" + feedId;
+    }
+
+}
---
+++ backend/chengzi/src/main/resources/run.sh
@@ -0,0 +1,3 @@
+java -jar chengzi-1.0.0-SNAPSHOT.jar --server.port=8081 >> /data/logs/chengzi_api.log 2>&1 &
+
+#http://localhost:8081/postfeed?feedId=324234234
